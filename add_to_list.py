76f9e7f8b048b22fdb7dbeca117eb419322d6fde
sabeerj31 <sabeerj3101@gmail.com>
2025-10-07 19:38:37 +0530

    learnt responsive widgets, padding,etc

--- add_to_list.py
+++ add_to_list.py
@@ -1,26 +1,52 @@
 import tkinter as tk
+from tkinter import ttk
 root = tk.Tk()
 root.title("Simple App")
-def add_to_list():
+def add_to_list(entry,list):
     text =  entry.get()
     if text:
-        text_list.insert(tk.END, text)
+        list.insert(tk.END, text)
         entry.delete(0,tk.END)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
 
-frame = tk.Frame(root)
-frame.grid(row=0, column=0)
+frame = ttk.Frame(root)
+frame.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
 
-entry = tk.Entry(frame)
+frame.columnconfigure(0, weight=1)
+frame.rowconfigure(1, weight=1)
+
+entry = ttk.Entry(frame)
 entry.grid(row=0, column=0)
 
-entry.bind("<Return>", lambda e: add_to_list())
+entry.bind("<Return>", lambda e: add_to_list(entry, text_list))
 
-entry_btn = tk.Button(frame, text="Add", command = add_to_list)
+entry_btn = ttk.Button(frame, text="Add", command =lambda: add_to_list(entry, text_list))
 entry_btn.grid(row=0, column=1)
 
 text_list = tk.Listbox(frame)
-text_list.grid(row=1, column=0)
+text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
+
+#frame2
+
+frame2 = tk.Frame(root)
+frame2.grid(row=0, column=1, sticky="nsew", padx=5, pady=5)
+
+frame2.columnconfigure(0, weight=1)
+frame2.rowconfigure(1, weight=1)
+
+entry2 = tk.Entry(frame2)
+entry2.grid(row=0, column=0)
+
+entry2.bind("<Return>", lambda e: add_to_list(entry2, text_list2))
+
+entry_btn2 = tk.Button(frame2, text="Add", command = lambda: add_to_list(entry2, text_list))
+entry_btn2.grid(row=0, column=1)
+
+text_list2 = tk.Listbox(frame2)
+text_list2.grid(row=1, column=0, columnspan=2, sticky="nsew")
 
 root.mainloop()
 
